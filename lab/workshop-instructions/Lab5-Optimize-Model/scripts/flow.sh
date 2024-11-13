ce96f3d8afea8aeb9686f474232935ebaf129e7c
Samuel Kemp <samuel100@users.noreply.github.com>
2024-11-13 10:37:55 +0000

    Update flow.sh
    updated flow script to llama

--- lab/workshop-instructions/Lab5-Optimize-Model/scripts/flow.sh
+++ lab/workshop-instructions/Lab5-Optimize-Model/scripts/flow.sh
@@ -1,37 +1,31 @@
 echo -e "\n>>>>>> running awq quantization >>>>>>>>\n"
 
 olive quantize \
-    --model_name_or_path azureml://registries/azureml/models/Phi-3.5-mini-instruct/versions/4 \
+    --model_name_or_path azureml://registries/azureml-meta/models/Llama-3.2-1B-Instruct/versions/1 \
     --algorithm awq \
-    --output_path models/phi/awq \
+    --output_path models/llama/awq \
     --log_level 1
 
 echo -e "\n>>>>>> running finetuning >>>>>>>>\n"
 
 olive finetune \
     --method lora \
-    --model_name_or_path models/phi/awq \
+    --model_name_or_path models/llama/awq \
     --trust_remote_code \
     --data_files "data/data_sample_travel.jsonl" \
     --data_name "json" \
     --text_template "<|user|>\n{prompt}<|end|>\n<|assistant|>\n{response}<|end|>" \
     --max_steps 15 \
-    --output_path ./models/phi/ft \
+    --output_path ./models/llama/ft \
     --log_level 1
 
-echo -e "\n>>>>>> running graph capture >>>>>>>>\n"
+echo -e "\n>>>>>> running optimizer >>>>>>>>\n"
 
-olive capture-onnx-graph \
-    --model_name_or_path models/phi/ft/model \
-    --adapter_path models/phi/ft/adapter \
+olive auto-opt \
+    --model_name_or_path models/llama/ft/model \
+    --adapter_path models/llama/ft/adapter \
+    --device cpu \
+    --provider CPUExecutionProvider \
     --use_ort_genai \
-    --output_path models/phi/onnx \
+    --output_path models/llama/onnx-ao \
     --log_level 1
-
-echo -e "\n>>>>>> generating adapters >>>>>>>>\n"
-
-olive generate-adapter \
-    --model_name_or_path models/phi/onnx \
-    --output_path models/phi/ft-ready \
-    --log_level 1
-
