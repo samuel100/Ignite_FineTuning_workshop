ff4fc397b01ade7e2363086feaa93830782fd4db
Kinfey <kinfeylo@microsoft.com>
2024-11-14 15:57:08 +0800

    Update lab7 samples

--- lab/workshop-instructions/Lab7_Consumption_in_app/readme.md
+++ lab/workshop-instructions/Lab7_Consumption_in_app/readme.md
@@ -13,7 +13,7 @@ By the end of this workshop, you should be able to:
 2. Compare the results of the models
 
 ## Lab Scenario
-Use ONNX Runtime for On-device, Use Azure AI for Cloud.
+This is console application
 
 
 ## Setup 
@@ -37,7 +37,7 @@ git clone https://github.com/Azure/Ignite_FineTuning_workshop.git
 
 ```
 cd Ignite_FineTuning_workshop/lab/workshop-instructions/Lab7_Consumption_in_app/scr
-ipts/ChatWithSLM
+ipts/ChatWithSLM.Console
 ```
 ## Open the solution in VScode 
 
@@ -47,116 +47,27 @@ code
 
 ## Running fine-tuned GPT model in the cloud 
 
-We will be using the previously created GPT-3.5 fined tuned model hosted in Azure AI
-You will need to make a note of the following settings from +++https://ai.azure.com+++
+Go to ChatWithSLM.Console/Utils/GenAI.cs , add your onnx models path, save
 
-The followng Endpoint, key and model name will be required in the next steps 
+![location](./images/location.png)
 
-```
-string aoai_endpoint = "Your AOAI Service Endpoint";        
-string aoai_key = "Your AOAI Service Key";        
-string aoai_model = "Your GPT-3.5 Fine tuning Deployment name";
-```
 
-## Running fine-tuned Phi local model application
-
-In this exercise, you will:
-
-- using ONNX Runtime GenAI to call fine-tuned model
-- using .NET Aspire to create local Copilot Solution
-
-### Objectives 
-
-1. Running the Fine Tuned Phi-3.5 Onnx Local Model with a .NET Aspire Application
-2. Running the Fine Tuned GPT-3.5 Model hosted in Azure AI Services 
-
-### Additional Resources 
-- Learn to set ONNX Runtime GenAI env [click here](../Additional_Labs/Local_Deployment_Model/readme.md)
--  Learn more about .NET Aspire env [click here](../Additional_Labs/dotNETAspire/readme.md)
-
-## Add Your Fine Tuned GPT3.5 Model Path and Azure OpenAI Endpoint 
-
-Go to '/scripts/ChatWithSLM/ChatWithSLM.API/Utils/GenAI.cs' , add ONNX Model Path
-
-```
-cd ./script/ChatWithSLM/ChatWithSLM.API/Utils/GenAI.cs
-
-```
-
-![onnx](./images/onnx.png)
+## Running console application
 
 
 ```
-cd ./scripts/ChatWithSLM/ChatWithSLM.UI/Utils/AOAIClient.cs
-```
-
-### Update the following with the details 
-
-```
-string aoai_endpoint = "Your AOAI Service Endpoint";        
-string aoai_key = "Your AOAI Service Key";        
-string aoai_model = "Your GPT-3.5 Fine tuning Deployment name";
-```
-
-![aoai](./images/aoai.png)
-
-
-## Running Scripts
-
-### Navigate to the Project Directory:
-
-```
-cd ./script/ChatWithSLM/ChatWithSLM.AppHost
-```
-
-### Build the Project:
-
-```
-dotnet dev-certs https --trust
 
 dotnet run
-```
-
-
-### Accessing the .NET Aspire Portal
-Click the Follow Link in the terminal to open the .NET Aspire Portal using the localhost link: 
-
-Eample of the follow link in the info section of the output in your terminal 
 
 ```
-Login to the dashboard at http://localhost:15147/login?t=65d752d2a8345d9f3t5656ef78e4777
-```
+
+You can choose two different scenarios
+
+1. Experience of different models based on travel data
+
+2. Experience of models before and after optimization
+
+As shown in the figure
 
 
-### Go to the Portal:
-
-Open up the brower windows and you will see the .NET Aspire Portal with a list of services and ports, select the chatwithslmui. You will see Endpoint as per the example below http://localhost:7012, click , you can go to the chat UI
-
-![portal](./images/portal.png)
-
-ChatUI
-
-![Chat](./images/chat.png)
-
-You can run Phi-3.5-Instruct ONNX Model , Phi-3.5-Finetuning-travel ONNX model,and GPT-3.5-Finetuing model 
-
-![Model](./images/model.png)
-
-Testing different model Result about "Where in Rome should I stay?"
-
-
-![Result](./images/result.png)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+![result](./images/result.png)
---
+++ lab/workshop-instructions/Lab7_Consumption_in_app/scripts/ChatSLM.Console/Program.cs
@@ -0,0 +1,152 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ChatSLM.Utils;
+
+// See https://aka.ms/new-console-template for more information
+
+
+void ShowMenu()
+{
+    Console.WriteLine("Please select an option:");
+    Console.WriteLine("1. Chat With Phi-3.5 & Phi-3.5-ft & GPT-3.5-ft with Travel Data");
+    Console.WriteLine("2. Evaluation Phi-3.5-ft ONNX Model");
+    Console.WriteLine("3. Exit");
+}
+
+void ChatWithModels()
+{
+    Console.WriteLine("Welcoment to ChatML Console! I am your ML assistant. Please ask me any question about travel. If you want to exit, please type ctrl + c.");
+
+    while (true)
+    {
+        Console.Write("Ask: ");
+        String? question = Console.ReadLine();
+
+        if (String.IsNullOrWhiteSpace(question))
+        {
+            Console.WriteLine("Bot : Please input a question.");
+            return;
+        }
+
+
+        Console.WriteLine("************************************************************************************************************" );
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Bot(Phi35): ");
+        Console.ResetColor();
+
+        GenAI.ChatWithGenAIONNXOpenAIAsync(1,question);
+
+        Console.WriteLine("************************************************************************************************************" );
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Bot(Phi35-ft): " );
+        Console.ResetColor();
+
+        GenAI.ChatWithGenAIONNXOpenAIAsync(2,question,true);
+
+        Console.WriteLine("************************************************************************************************************" );
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Bot(GPT35-ft): " );
+        Console.ResetColor();
+
+        Console.WriteLine(GenAI.ChatWithAOAI(question));
+
+        Console.WriteLine("************************************************************************************************************" );
+
+
+    }
+}
+
+void EvaluateModel()
+{
+    Console.WriteLine("Welcoment to ChatML Console! You can compare different models . If you want to exit, please type ctrl + c.");
+
+    while (true)
+    {
+        Console.Write("Ask: ");
+        String? question = Console.ReadLine();
+
+        if (String.IsNullOrWhiteSpace(question))
+        {
+            Console.WriteLine("Bot : Please input a question.");
+            return;
+        }
+
+
+        Console.WriteLine("************************************************************************************************************" );
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Bot(Phi35-ft): ");
+        Console.ResetColor();
+
+        GenAI.ChatWithGenAIONNXOpenAIAsync(2,question,true);
+
+        Console.WriteLine("************************************************************************************************************" );
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Bot(optimization-Phi35-ft): " );
+        Console.ResetColor();
+
+        GenAI.ChatWithGenAIONNXOpenAIAsync(3,question,true);
+
+        Console.WriteLine("************************************************************************************************************" );
+
+
+    }
+    // Add your evaluation logic here
+}
+
+while (true)
+{
+    ShowMenu();
+    Console.Write("Select an option: ");
+    String? option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            ChatWithModels();
+            break;
+        case "2":
+            EvaluateModel();
+            break;
+        case "3":
+            return;
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+}
+
+// Console.WriteLine("Welcoment to ChatML Console! I am your ML assistant. Please ask me any question about ML and AI. If you want to exit, please type ctrl + c.");
+
+// while(true){
+
+//     Console.Write("Ask: ");
+//     String? question = Console.ReadLine();
+
+//     if (String.IsNullOrWhiteSpace(question))
+//     {
+//         Console.WriteLine("Bot : Please input a question.");
+//         return;
+//     }
+
+//     Console.WriteLine("************************************************************************************************************" );
+
+//     Console.Write("Bot(Phi35): " );
+
+//     GenAI.ChatWithGenAIONNXOpenAIAsync(question);
+
+//     Console.WriteLine("************************************************************************************************************" );
+
+
+//     Console.Write("Bot(Phi35-ft): " );
+
+//     GenAI.ChatWithGenAIONNXOpenAIAsync(question,true);
+
+//     Console.WriteLine("************************************************************************************************************" );
+
+// }
